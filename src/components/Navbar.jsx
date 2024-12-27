a5b6fb558e074788d19b5d174da598272b96673d
MarziaJalili <astronomyfancy@gmail.com>
2024-12-27 17:30:40 +0430

    fixed the error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,19 +6,19 @@ function Navbar() {
     <nav className="ping-container__navbar">
       <ul className="ping-container__navigation-gird">
         <li>
-          <a href="#">
+          <a href="#" aria-label="Visit Example Website">
             <i className="fa-brands fa-facebook-f"></i>
           </a>
         </li>
 
         <li>
-          <a href="#">
+          <a href="#" aria-label="Visit Example Website">
             <i className="fa-brands fa-twitter"></i>
           </a>
         </li>
 
         <li>
-          <a href="#">
+          <a href="#" aria-label="Visit Example Website">
             <i className="fa-brands fa-instagram"></i>
           </a>
         </li>
